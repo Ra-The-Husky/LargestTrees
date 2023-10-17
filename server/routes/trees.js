a7ff91c96cbae74ef674d1b993f2439319940f78
Rasheed Lindsey <rasheedlindsey@live.com>
2023-10-17 19:58:12 -0400

    Practice Complete?

---
+++ server/routes/trees.js
@@ -0,0 +1,87 @@
+// Instantiate router - DO NOT MODIFY
+const express = require("express");
+const router = express.Router();
+
+/**
+ * BASIC PHASE 2, Step A - Instantiate SQLite and database
+ *   - Database file: "data_source" environment variable
+ *   - Database permissions: read/write records in tables
+ */
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database(
+  process.env.DATA_SOURCE,
+  sqlite3.OPEN_READWRITE
+);
+
+// BASIC PHASE 2, Step B - List of all trees in the database
+
+// Protocol: GET
+// Path: /
+router.get('/trees', (req, res, next) => {
+
+  console.log('Handling GET request for /trees')
+  const sql = "SELECT * FROM trees";
+  // Parameters: None
+  const params = [];
+
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      next(err);
+    } else {
+      //  Response: JSON array of objects
+      //  Object properties: height-ft, tree, id
+      //  Ordered by the height_ft from tallest to shortest
+      res.json(rows);
+    }
+  });
+});
+
+/**
+ * BASIC PHASE 3 - Retrieve one tree with the matching id
+ *
+ * Path: /:id
+ * Protocol: GET
+ * Parameter: id
+ * Response: JSON Object
+ *   - Properties: id, tree, location, height_ft, ground_circumference_ft
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 4 - INSERT tree row into the database
+ *
+ * Path: /trees
+ * Protocol: POST
+ * Parameters: None
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 5 - DELETE a tree row from the database
+ *
+ * Path: /trees/:id
+ * Protocol: DELETE
+ * Parameter: id
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+/**
+ * INTERMEDIATE PHASE 6 - UPDATE a tree row in the database
+ *
+ * Path: /trees/:id
+ * Protocol: PUT
+ * Parameter: id
+ * Response: JSON Object
+ *   - Property: message
+ *   - Value: success
+ */
+// Your code here
+
+// Export class - DO NOT MODIFY
+module.exports = router;
---
+++ server/seed-data.sql
@@ -0,0 +1,20 @@
+-- BASIC PHASE 1A - DROP and CREATE table
+DROP TABLE IF EXISTS trees;
+
+CREATE TABLE trees (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    tree VARCHAR(32),
+    location VARCHAR(64),
+    height_ft DECIMAL(3,1),
+    ground_circumference_ft DECIMAL(3,1)
+);
+
+-- BASIC PHASE 1B - INSERT seed data
+INSERT INTO trees (tree, location, height_ft, ground_circumference_ft)
+VALUES
+('General Sherman', 'Sequoia National Park', 274.9, 102.6),
+('General Grant', 'Kings Canyon National Park', 268.1, 107.5),
+('President', 'Sequoia National Park', 268.1, 107.5),
+('Lincoln', 'Sequoia National Park', 255.8, 98.3),
+('Stagg', 'Private Land', 243.0, 109.0);
+
